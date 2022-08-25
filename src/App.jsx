ddcf2304b06510bafae4ede76ac4b34f9177309f
JP Grineau <jgrineau95@gmail.com>
2022-08-25 12:19:24 -0400

    Initialize project using Create React App Update the components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+
+    </>
+  )
+}
+
+export default App
