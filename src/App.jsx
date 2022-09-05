d42ed7e627fcbccaf549c32da4de18ee157c862b
JGrineau <jgrineau95@gmail.com>
2022-09-05 16:57:28 -0400

    Update of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import About from './components/about/About'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
     <Header />
     <Nav />
+    <About />
   
 
     </>
