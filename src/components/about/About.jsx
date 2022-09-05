bb792a54355f73717559fd5e487e67d0e4b2abda
JGrineau <jgrineau95@gmail.com>
2022-09-05 16:42:27 -0400

    Testing adding to a branch for pull

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { FaAward } from 'react-icons/fa';
+import { VscFolderLibrary } from 'react-icons/vsc';
 import './about.css'
 
-const about = () => {
+const About = () => {
   return (
     <section id='about'>
       <h5>Learn</h5>
       <h1>About Me</h1>
+      <h1>Testing the git branch changes and issues</h1>
     </section>
   )
 }
 
-export default about
+export default About
