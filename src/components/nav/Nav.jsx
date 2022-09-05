fcede53c5d8177c9b22e1eb489a81a65163b47f5
JGrineau <jgrineau95@gmail.com>
2022-09-05 15:26:43 -0400

    Fixed nav bar active push

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><BsBook /></a>
-      <a href='#contact'><BiMessageDetail /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail /></a>
       
     </nav>
   )
