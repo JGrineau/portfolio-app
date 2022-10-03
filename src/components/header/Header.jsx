36b2f08c5b096cdc1651932fa49f2dc20b258f13
JGrineau <jgrineau95@gmail.com>
2022-10-03 13:54:40 -0400

    hero update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
+
 import './header.css'
+import background from '../../assets/dots.mp4'
 import CTA from './CTA'
+
 
 
 const Header = () => {
   return (
-    <section>
-      <div className="hero">
-        <h5>Hello I'm</h5>
-        <h1>JP Grineau</h1>
-        <h5 className="title">Software Developer</h5>
+      <div className="main">
+        
+        <video src={background} autoPlay loop muted />
+        <div className="hero">
+          <h1>JP Grineau</h1>
+          <h5>Software Developer</h5>
+        </div>
         <CTA />
-        
-        <a href="#contact" className="scroll__down">Scroll Down</a>
-        
+    
       </div>
-    </section>
   )
 }
 
