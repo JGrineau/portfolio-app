5275709abe4c4faa3d9f364cf1e85c9e49eb9f42
JGrineau <jgrineau95@gmail.com>
2022-10-03 20:11:38 -0400

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/header/Header'
-import HeaderSocials from './components/header/HeaderSocials'
-import Nav from './components/nav/Nav'
+// import HeaderSocials from './components/header/HeaderSocials'
+// import Nav from './components/nav/Nav'
 import Navbar from './components/Navbar/Navbar'
 import About from './components/about/About'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -11,10 +11,10 @@ const App = () => {
   return (
     
    <Router>
-    <Nav />
+    
       <Navbar />
       <Header />
-      <HeaderSocials />
+      
       <About />
     </Router>
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BsBook} from 'react-icons/bs'
-import {BiMessageDetail} from 'react-icons/bi'
+// import {AiOutlineHome} from 'react-icons/ai'
+// import {AiOutlineUser} from 'react-icons/ai'
+// import {BsBook} from 'react-icons/bs'
+// import {BiMessageDetail} from 'react-icons/bi'
 import { useState } from 'react'
-import LOGO from '../../assets/LOGO.png';
+// import LOGO from '../../assets/LOGO.png';
 
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <div className='nav'>
-    <div className='nav-center'>
+    {/* <div className='nav-center'>
 
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
@@ -21,7 +21,7 @@ const Nav = () => {
 
     </div>
       
-      
+       */}
       </div>
   )
 }
