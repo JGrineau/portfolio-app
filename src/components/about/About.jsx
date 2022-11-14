efbdbf5b019ea14ab9a7cfd0620c15a9cafdd000
JGrineau <jgrineau95@gmail.com>
2022-11-14 00:02:38 -0500

    About page small update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './about.css'
+import logo from '../../assets/logo_red.png'
+
+
+
 
 const About = () => {
   return (
@@ -11,8 +15,9 @@ const About = () => {
       <p> I'm a very ambitious front-end web developer looing for a role in establiushed IT company with the opportunity to work with the latesttechnologies on challenging and diverse projects.</p>
       <p> Be fearless in the pursuit of what sets your sole on fire ~ Unknown</p>
       </div>
-      
-
+      <img className='logo__about'
+                src={logo} alt="Logo"
+            />
       </div>
   
     
