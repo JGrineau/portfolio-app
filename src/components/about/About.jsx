63f268e09ff6af4431ea3bc946c33079eb610305
JGrineau <jgrineau95@gmail.com>
2022-11-14 20:32:19 -0500

    about page update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,23 +1,47 @@
 import React from 'react'
 import './about.css'
 import logo from '../../assets/logo_red.png'
+import {BsFillPlayFill} from 'react-icons/bs'
 
 
 
 
 const About = () => {
   return (
-    <div className='about'>
+    <div className='aboutContainer'>
+      
       <div className='text-zone'>
         <h1>About Me</h1>
       
       <p> I'm a very ambitious front-end web developer looing for a role in establiushed IT company with the opportunity to work with the latesttechnologies on challenging and diverse projects.</p>
       <p> I'm a very ambitious front-end web developer looing for a role in establiushed IT company with the opportunity to work with the latesttechnologies on challenging and diverse projects.</p>
       <p> Be fearless in the pursuit of what sets your sole on fire ~ Unknown</p>
+
+      <div className='tech'>
+        <p>Here are a few technologies I have recently been working with:</p>
+              <ul>
+                <li><BsFillPlayFill /> JavaScript</li>
+                <li><BsFillPlayFill /> JavaScript</li>
+                <li><BsFillPlayFill /> JavaScript</li>
+                <li><BsFillPlayFill /> JavaScript</li>
+                <li><BsFillPlayFill /> JavaScript</li>
+                <li><BsFillPlayFill /> JavaScript</li>
+              </ul>
+            </div>
       </div>
+
+      
+
+      <section className='imageSection'>
       <img className='logo__about'
-                src={logo} alt="Logo"
+                src={logo} alt="Logo" 
             />
+            </section>
+
+             
+            
+            
+            
       </div>
   
     
