86202ea1b3b2778fec98ac2ded1a80071dbb3ea6
JGrineau <jgrineau95@gmail.com>
2023-09-04 15:06:24 -0400

    Marquee Update

--- src/components/marquee/Marquee.jsx
+++ src/components/marquee/Marquee.jsx
@@ -1,37 +1,54 @@
-import React from 'react'
-import './marquee.css';
+import React from "react";
+import "./marquee.css";
+
+// const Marquee = () => {
+
+//   return (
+//     <div className="marquee__container">
+//     <div className='marquee'>
+//       <ul className='marquee__content'>
+//         <li>BE</li>
+//         <li>FEARLESS  </li>
+//         <li>IN</li>
+//         <li>THE</li>
+//         <li>PURSUIT</li>
+//         <li>OF</li>
+//         <li>WHAT</li>
+//         <li>SETS</li>
+//         <li>YOUR</li>
+//         <li>SOUL</li>
+//         <li>ON</li>
+//         <li>FIRE</li>
+
+//         <li>BE</li>
+//         <li>FEARLESS</li>
+//         <li>IN</li>
+//         <li>THE</li>
+
+//       </ul>
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default Marquee
 
 const Marquee = () => {
-
   return (
-    <div className="marquee__container">
-    <div className='marquee'>
-      <ul className='marquee__content'>
-        <li>BE</li>
-        <li>FEARLESS  </li>
-        <li>IN</li>
-        <li>THE</li>
-        <li>PURSUIT</li>
-        <li>OF</li>
-        <li>WHAT</li>
-        <li>SETS</li>
-        <li>YOUR</li>
-        <li>SOUL</li>
-        <li>ON</li>
-        <li>FIRE</li>
+    <div class="marquee marquee--hover-pause">
+      <ul class="marquee__content">
+        <li>All</li>
+        <li>Code</li>
+        <li>Everything</li>
+      </ul>
 
-        <li>BE</li>
-        <li>FEARLESS</li>
-        <li>IN</li>
-        <li>THE</li>
-        
-        
-       
-
+      <ul class="marquee__content" aria-hidden="true">
+        <li>All</li>
+        <li>Code</li>
+        <li>Everything</li>
       </ul>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Marquee
+export default Marquee;
