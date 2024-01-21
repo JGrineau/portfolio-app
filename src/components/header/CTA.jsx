2db6e2677f3db7b91256ab1a179cde538c04e5da
JP Grineau <jgrineau95@gmail.com>
2024-01-21 16:06:11 -0500

    Update to resume download

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import CV from '../../assets/JP_Grineau_Resume.pdf'
-import Button from '../button/Button'
-
+import React from "react";
+import CV from "../../assets/JP_Resume.pdf";
+import Button from "../button/Button";
 
 const CTA = () => {
   return (
     <div className="cta">
-  
-     <a href={CV} download>
-      <Button
-      type="button"
-      buttonStyle="btn--primary"
-      buttonSize="btn--large"
-      >Download CV</Button>
+      <a href={CV} download>
+        <Button
+          type="button"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Download CV
+        </Button>
       </a>
 
       <a href="mailto:jgrineau95@gmail.com">
-      <Button
-      type="button"
-      buttonStyle="btn--secondary"
-      buttonSize="btn--large"
-      >Email Me</Button>
+        <Button
+          type="button"
+          buttonStyle="btn--secondary"
+          buttonSize="btn--large"
+        >
+          Email Me
+        </Button>
       </a>
-      
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
