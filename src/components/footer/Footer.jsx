53efc7ddaefb3c8f9d8675b84aa36e2cdb3964db
JP Grineau <jgrineau95@gmail.com>
2024-01-21 16:08:55 -0500

    Update to year of website

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
-import {BsInstagram} from 'react-icons/bs'
-import Marquee from '../marquee/Marquee';
-import './footer.css';
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import Marquee from "../marquee/Marquee";
+import "./footer.css";
 
 const Footer = () => {
-
-  
-
   return (
     <footer>
       <Marquee />
-      <a href="#home" className="footer__logo">JPG</a>
+      <a href="#home" className="footer__logo">
+        JPG
+      </a>
       <ul className="links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
       </ul>
       <div className="footer__socials">
-      <a href='https://www.linkedin.com/in/jpgrineau/' target="_blank" rel="noreferrer"><BsLinkedin /></a>
-        
-        
-            <a href='https://github.com/JGrineau?tab=repositories' target="_blank" rel="noreferrer"><BsGithub /></a>
-        
-        
-            <a href='https://www.instagram.com/' target="_blank" rel="noreferrer"><BsInstagram /></a>
+        <a
+          href="https://www.linkedin.com/in/jpgrineau/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com/JGrineau?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsGithub />
+        </a>
+
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+          <BsInstagram />
+        </a>
       </div>
       <div className="footer__copyright">
-        <small>&copy; JP Grineau 2023.</small>
+        <small>&copy; JP Grineau 2024.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
